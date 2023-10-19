b557c0d4f0f485a5927bcf3c633a73f4917ab305
HRIDOY CHANDRA  SAHA <145109607+hridoy11111111@users.noreply.github.com>
2023-10-19 10:35:18 +0600

    rr

---
+++ index.js
@@ -0,0 +1,86 @@
+var hridoy = 100;
+hridoy +=500;
+// 
+var h = "dhaka";
+ var a = "cumilla";
+//  console.log(a);
+var friend = true;
+
+// console.log(typeof (h));
+var isWeatherSunny = false;
+
+var isEshikhonOpen = "Open";
+// 
+var i = 500;
+var t = 300;
+// console.log(i+t);
+var name = 'hridoy'
+// console.log(typeof(name));
+
+// var total = name + " " + nam;
+// console.log(total);
+var eggPrice = 15;
+var oilPrice = 100;
+
+var totalPrice = eggPrice + oilPrice;
+// console.log(totalPrice);
+var friend  = true;
+var girlFriend = false;
+// console.log(typeof (girlFriend));
+
+
+var bataShoe = 999;
+var sock =50;
+
+var shoeTotal = bataShoe + sock;
+// console.log(shoeTotal);
+var chicken =99.9999;
+var convert = parseInt(chicken) 
+// var oil = 100;
+// var superMarket = convert + oil;
+// console.log(convert);  
+var ammount = 100;
+var people = 10;
+
+var add = ammount + people;
+var minus = ammount - people;
+var multi = ammount * people;
+var division = ammount / people;
+var a =ammount - people;
+var a =ammount*people;
+var p=ammount/people;
+// console.log(p);
+var name="saha Hridoy";
+name="Hridoy saha";
+// console.log(name);
+
+var price=88;
+price=88 + 2;
+// console.log(typeof(price));
+price +=5;
+// console.log(price)
+var age=20;
+age=20+1;
+age+=1;
+age-=1;
+age*=2;
+age/=2;
+// console.log(age);
+var age=15;
+age**
+age--
+// console.log(age)
+ var price=100;
+
+
+
+
+var today=new Date();
+// console.log(today);
+var number1=0.1;
+var number2=0.2;
+var total = number1+number2;
+
+ var solve = parseFloat(total.toFixed(1));
+
+console.log(solve);
